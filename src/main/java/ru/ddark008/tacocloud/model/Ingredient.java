1f78a20b6ade66e5130cd21f08e15714bbaae76b
Eliseev D <i@ddark008.ru>
2023-01-28 10:57:12 +0300

    fix(ch3.1): record not serializable

--- src/main/java/ru/ddark008/tacocloud/model/Ingredient.java
+++ src/main/java/ru/ddark008/tacocloud/model/Ingredient.java
@@ -1,6 +1,17 @@
 package ru.ddark008.tacocloud.model;
 
-public record Ingredient(String id, String name, Type type) {
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class Ingredient {
+    String id;
+    String name;
+    Type type;
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
